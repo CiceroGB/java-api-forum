26647d916797968a4bdb371b0f682a54d1921060
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-07-24 12:03:01 -0300

    Handle message error on validation

---
+++ src/main/java/com/cicerogb/forum/config/validation/ErrorDto.java
@@ -0,0 +1,20 @@
+package com.cicerogb.forum.config.validation;
+
+public class ErrorDto {
+
+	private String field;
+	private String error;
+	public ErrorDto(String field, String error) {
+		this.field = field;
+		this.error = error;
+	}
+	public String getField() {
+		return field;
+	}
+	public String getError() {
+		return error;
+	}
+	
+	
+	
+}
